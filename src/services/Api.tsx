0767e6d5d8df20a388768227cbf80047f7a113b7
aswathyrenfos <aswathy@renfos.com>
2025-07-31 07:17:09 +0530

    live url

--- src/services/Api.tsx
+++ src/services/Api.tsx
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from 'axios';
 
 const axiosInstance: AxiosInstance = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api/', // Set your base URL here
+  baseURL: 'https://demorealestate.webnapps.net/api/', // Set your base URL here
 });
 
 // Request interceptor to add headers
